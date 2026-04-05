23e204a69f50b03a2a189c2c9d138a21715c463b
Rhishamah <rhishamah28@gmail.com>
2026-04-05 14:10:55 +0100

    created new model classes implemented one to many relationships among the model classes and corrected  some typographical errors in the code

--- store/models.py
+++ store/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class product(models.model):
+
+
+
+class Collection(models.Model):
+        title = models.CharField(max_length=255)
+
+class Item(models.Model):
+        title = models.CharField(max_length=255)
+
+class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
 
-class customer(models.model):
+
+class Customer(models.Model):
     MEMBERSHIP_BRONZE = "B"
     MEMBERSHIP_SILVER = "S"
     MEMBERSHIP_GOLD = "G"
@@ -25,10 +36,11 @@ class customer(models.model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
-    class Order(models.model):
+
+class Order(models.Model):
         PAYMENT_STATUS_PENDING = "P"
-        PAYMENT_STATUS_PENDING = "C"
-        PAYMENT_STATUS_PENDING = "F"
+        PAYMENT_STATUS_COMPLETE = "C"
+        PAYMENT_STATUS_FAILED = "F"
         PAYMENT_STATUS_CHOICES =[
             ("PAYMENT_STATUS_PENDING", "Pending"),
             ("PAYMENT_STATUS_COMPLETE", "Complete"),
@@ -36,3 +48,24 @@ class customer(models.model):
         ]
         placed_at = models.DateTimeField(auto_now_add=True)
         payment_status= models.CharField(max_length=255, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+        Customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+            order = models.ForeignKey(Order, on_delete=models.PROTECT)
+            product = models.ForeignKey(Product, on_delete=models.PROTECT)
+            quantity = models.PositiveSmallIntegerField()
+            unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+
+class Address(models.Model) :
+            street = models.CharField(max_length=255)
+            city = models.CharField(max_length=255)
+            customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+            item = models.ForeignKey(Item,on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+        cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+        product = models.ForeignKey(Product, on_delete=models.CASCADE)
+        quantity = models.PositiveSmallIntegerField()
