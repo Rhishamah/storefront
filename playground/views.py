b8c6ae177cfc4ba8069a64003d10b06074446816
Rhishamah <rhishamah28@gmail.com>
2026-04-13 00:44:00 +0100

    Refactor say_hello view to handle ObjectDoesNotExist exception

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
+
 
 # Create your views here.
 
 def say_hello(request):
-    product = Product.objects.get(pk=1)
-    
+    try:
+        product = Product.objects.get(pk=0)
+    except ObjectDoesNotExist:
+        pass
 
     return render(request, "hello.html",{"name":"vincent"})
