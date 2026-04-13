262d890b9a4d27d207ee589d1973e6fb5c1d64ce
Rhishamah <rhishamah28@gmail.com>
2026-04-13 16:40:56 +0100

    deferring  fields

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q, F
 from store.models import Product
-from store.models import OrderItem
 
 
 # Create your views here.
 
 def say_hello(request):
     # sorting data 
-    queryset =  Product.objects.filter(
-        id__in=OrderItem.objects.values("product_id").distinct()).order_by("title")
+    queryset =  Product.objects.only("id", "title")
 
 
     return render(request, "hello.html",{"name":"vincent", "products": list(queryset)})
