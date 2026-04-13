7299007de511a960800120d8434d0c3d4e37bd3f
Rhishamah <rhishamah28@gmail.com>
2026-04-13 10:45:58 +0100

    Add Q object import to views.py for complex query support

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
 
