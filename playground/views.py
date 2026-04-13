72e293deb2ead85f7430be135e817a9400accc05
Rhishamah <rhishamah28@gmail.com>
2026-04-13 16:34:18 +0100

    specifying query fields

--- playground/views.py
+++ playground/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
 from store.models import Product
+from store.models import OrderItem
 
 
 # Create your views here.
 
 def say_hello(request):
     # sorting data 
-    queryset = Product.objects.values("id", "title", "collection__title")
+    queryset =  Product.objects.filter(
+        id__in=OrderItem.objects.values("product_id").distinct()).order_by("title")
+
 
     return render(request, "hello.html",{"name":"vincent", "products": list(queryset)})
