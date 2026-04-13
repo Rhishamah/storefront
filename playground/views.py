5a97e51e21c16488ff47f7474a2da41f8dc0ea7f
Rhishamah <rhishamah28@gmail.com>
2026-04-13 16:12:20 +0100

    limiting queries

--- playground/views.py
+++ playground/views.py
@@ -8,6 +8,6 @@ from store.models import Product
 
 def say_hello(request):
     # sorting data 
-    queryset = Product.objects.all()[:5] 
+    queryset = Product.objects.all()[5:10] 
 
     return render(request, "hello.html",{"name":"vincent", "products": list(queryset)})
