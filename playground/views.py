a1fae12f35a7b467a7c66bef50ceb55de04f158c
Rhishamah <rhishamah28@gmail.com>
2026-04-14 12:04:45 +0100

    Another method for annotating objects by referencing them

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Value
+from django.db.models import Value, F
 from store.models import Product, Customer
 
 
 # Create your views here.
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(is_new=Value(True))
+    queryset = Customer.objects.annotate(new_id=F("id"))
 
     return render(request, "hello.html",{"name":"vincent", "result":list(queryset)})
