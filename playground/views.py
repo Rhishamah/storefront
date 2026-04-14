25c9c924b60a8af972ffcada57c819e3ab6155b3
Rhishamah <rhishamah28@gmail.com>
2026-04-14 11:34:23 +0100

    Aggregating objects

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product
-from store.models import Order
-from store.models import OrderItem
 
 
 # Create your views here.
 
 def say_hello(request):
-    # select_related is used when the other end of the relationship has only one instance (1)
-    # prefetch_related is used when the  other end of the relationship has many objects (n)
-    queryset =  Order.objects.select_related(
-        "customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
+    result =  Product.objects.aggregate(count=Count("id"), min_price=Min("unit_price"))
 
-
-    return render(request, "hello.html",{"name":"vincent", "orders": list(queryset)})
+    return render(request, "hello.html",{"name":"vincent", "result":result})
