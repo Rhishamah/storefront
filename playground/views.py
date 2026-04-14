ffc6192b007d0874ec08e5ed63a47841ec0b6b23
Rhishamah <rhishamah28@gmail.com>
2026-04-14 11:04:11 +0100

    update product query to include select_related for collection

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
+from store.models import Order
+from store.models import OrderItem
 
 
 # Create your views here.
@@ -8,7 +10,7 @@ from store.models import Product
 def say_hello(request):
     # select_related is used when the other end of the relationship has only one instance (1)
     # prefetch_related is used when the  other end of the relationship has many objects (n)
-    queryset =  Product.objects.prefetch_related("promotions").all()
+    queryset =  Product.objects.prefetch_related("promotions").select_related("collection").all()
 
 
     return render(request, "hello.html",{"name":"vincent", "products": list(queryset)})
