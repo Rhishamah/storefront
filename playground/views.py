0819153a0e5621147d16ef7efaca858fa9d129b6
Rhishamah <rhishamah28@gmail.com>
2026-04-14 12:42:18 +0100

    importing the CONCAT  class to import database functions

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Value, F, Func
+from django.db.models.functions import Concat
 from store.models import Product, Customer
 
 
@@ -10,5 +11,12 @@ def say_hello(request):
     queryset = Customer.objects.annotate(
         full_name=Func(F("first_name"), Value(" "), F("last_name"), function="CONCAT")
     )
+
+#shortcut instead of using the first method 
+def say_hello(request):
+    queryset = Customer.objects.annotate(
+    full_name = Concat("first_name", Value(" "), "last_name"))
+    
+
 
     return render(request, "hello.html",{"name":"vincent", "result":list(queryset)})
