75428627399f2f01a62252fd08068e2d9ff39323
Rhishamah <rhishamah28@gmail.com>
2026-04-17 13:24:17 +0100

    creating objects to insert records in the database

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.contenttypes.models import ContentType
-from store.models import Product
-from tags.models import TaggedItem
+from store.models import Order, OrderItem, Product, Customer, Collection
 
 # Create your views here.
 
 def say_hello(request):
-    
-
+    collection = Collection()
+    collection.title = "Video Games"
+    collection.featured_product = Product(pk=1)
+    collection.save()
 
     return render(request, "hello.html",{"name":"vincent"})
