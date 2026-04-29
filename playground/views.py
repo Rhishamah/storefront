9bf19ad02403a3783aded6190463f81a2d99d85d
Rhishamah <rhishamah28@gmail.com>
2026-04-29 14:37:29 +0100

    deleting objects

--- playground/views.py
+++ playground/views.py
@@ -5,9 +5,9 @@ from store.models import Order, OrderItem, Product, Customer, Collection
 # Create your views here.
 
 def say_hello(request):
-    # collection = Collection.objects.get(pk=11)
-    # collection.featured_product = None
-    # collection.save()
+    collection = Collection(pk=10)
+    collection.delete()
+    # or for multiple objects
+    Collection.objects.filter(id__gt=5).delete()
 
-    Collection.objects.filter(pk=10).update(featured_product=None)
     return render(request, "hello.html",{"name":"vincent"})
