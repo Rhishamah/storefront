76d2d5575a354bb023e4fc63c8bc8603a39c15ad
Rhishamah <rhishamah28@gmail.com>
2026-04-29 14:33:55 +0100

    updating objects

--- playground/views.py
+++ playground/views.py
@@ -5,9 +5,9 @@ from store.models import Order, OrderItem, Product, Customer, Collection
 # Create your views here.
 
 def say_hello(request):
-    collection = Collection(pk=10)
-    collection.title = "Video Games"
-    collection.featured_product = None
-    collection.save()
+    # collection = Collection.objects.get(pk=11)
+    # collection.featured_product = None
+    # collection.save()
 
+    Collection.objects.filter(pk=10).update(featured_product=None)
     return render(request, "hello.html",{"name":"vincent"})
