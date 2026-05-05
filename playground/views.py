28fca8a6cf8c917e7e95217c7994303482bcd6a7
Rhishamah <rhishamah28@gmail.com>
2026-05-05 14:41:42 +0100

    created superuser added  changed index and admin title name

--- playground/views.py
+++ playground/views.py
@@ -6,9 +6,6 @@ from store.models import Order, OrderItem, Product, Customer, Collection
 # Create your views here.
 
 def say_hello(request):
-    with connection.cursor() as cursor:
-        cursor.execute()
-        # for executing stored procedures 
-        cursor.callproc("get_customers", [1,2,3, 'a'])
 
-    return render(request, "hello.html",{"name":"vincent", "result": list(queryset)})
+
+    return render(request, "hello.html",{"name":"vincent"})
