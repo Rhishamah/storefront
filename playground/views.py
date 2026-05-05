047552607c3b727763fbeda4c6c7f89e98e71eb1
Rhishamah <rhishamah28@gmail.com>
2026-05-05 13:53:38 +0100

    using the cursor package for custom sql statements

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import connection
 from django.http import HttpResponse
 from store.models import Order, OrderItem, Product, Customer, Collection
 
 # Create your views here.
 
 def say_hello(request):
-    queryset = Product.objects.raw("SELECT id, title FROM store_product")
-
+    with connection.cursor() as cursor:
+        cursor.execute()
+        # for executing stored procedures 
+        cursor.callproc("get_customers", [1,2,3, 'a'])
 
     return render(request, "hello.html",{"name":"vincent", "result": list(queryset)})
